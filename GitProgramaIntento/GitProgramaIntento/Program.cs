66454f3985bd81627bb58517f0443dd1e3de6de5
Javinator11 <sosa.puga.javier@gmail.com>
2019-04-22 23:10:37 -0600

    Termindo?

--- GitProgramaIntento/GitProgramaIntento/Program.cs
+++ GitProgramaIntento/GitProgramaIntento/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace GitProgramaIntento
 {
@@ -10,6 +11,27 @@ namespace GitProgramaIntento
     {
         static void Main(string[] args)
         {
+            string num = File.ReadAllText(@"D:\Programas 2ndo Semestre\Repositorio1\GitProgramaIntento\Numeross.txt");
+            string[] num2 = num.Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] numeros = new int[num2.Length];
+
+            for (int i = 0; i <= numeros.Length; i++)
+            {
+                numeros[i] = int.Parse(num2[i]); 
+
+                if(numeros[i]%10 == 0)
+                {
+                    Console.WriteLine(numeros[i] + " es multiplo de 10 ");
+                }
+
+            }
+            
+            
+
+            //for (int i = 0; i < num2.Length; i++)
+            //{
+            //    Console.WriteLine(num[i]);
+            //}
         }
     }
 }
